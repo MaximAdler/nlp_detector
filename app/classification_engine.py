b4fe38a5aae682bcbb7e80cfa69c782b290dd6ec
MaximAdler <orelmax@i.ua>
2019-10-13 16:55:46 +0300

    added json storage engine

---
+++ app/classification_engine.py
@@ -0,0 +1,30 @@
+import json
+
+
+class Storage:
+
+    def __init__(self, path: str, encoding: str = 'utf-8'):
+        self.path = path
+        self.encoding = encoding
+        self._data = None
+
+    def __enter__(self):
+        return self.read()
+
+    def __exit__(self, *args, **kwargs):
+        self._data = None
+
+    def read(self) -> 'Storage':
+        with open(self.path, encoding=self.encoding) as json_file:
+            self._data = json.load(json_file)
+        return self
+
+    def write(self) -> 'Storage':
+        with open(self.path, 'w', encoding=self.encoding) as json_file:
+            json.dump(self._data, json_file)
+        return self
+
+    @property
+    def data(self) -> dict:
+        return self._data
+
